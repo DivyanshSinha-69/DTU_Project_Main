4d5dd5f1e2b781f40d7809bb44ed05138fa7fbb3
Amarnath Yadav <amar312696@gmail.com>
2023-12-21 22:35:27 +0530

    checking 123

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express=require("express")
 const cors=require("cors");
 const app=express();
-app.listen(5000,()=>{
+
+
+app.get('/route',(req,res)=>{
+    res.send("hello world");
+})
+app.listen(3000,()=>{
     console.log("server started");
 });
