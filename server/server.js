e51c04538240a42f24c6d60e6fde068a378efaf1
arpangoyal <arpangoyal2002@gmail.com>
2023-12-21 22:18:53 +0530

    ini2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,33 @@
-const express=require("express")
-const cors=require("cors");
-const app=express();
-app.listen(5000,()=>{
-    console.log("server started");
+const express = require("express");
+const cors = require("cors");
+const BodyParser = require("body-parser");
+require("dotenv/config");
+
+const Port = process.env.PORT;
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+//Routes
+
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Content-Type': 'application/json"
+  );
+  next();
+});
+app.use(BodyParser.json());
+
+app.listen(Port,() => {
+  console.log(`server running at ${Port}`);
 });
