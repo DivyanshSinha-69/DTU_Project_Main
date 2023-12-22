9396b405765c2dd6e8f2d4ae63d152a3b720a49b
arpangoyal <arpangoyal2002@gmail.com>
2023-12-22 23:14:33 +0530

    file setup completed

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,31 +1,13 @@
-const Router = require("express").Router();
+import express from "express";
+import { getMyProfile, login, logout, register } from "../controllers/admin.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
-Router.post("/", async (req, res) => {
-  try {
-    const { email, password, secret } = req.body;
-    const { EMAIL, PASSWORD, SECRET_CODE } = process.env;
-    if (email !== EMAIL && password !== PASSWORD && secret !== SECRET_CODE) {
-      res.send("Please Check Data");
-      return;
-    }
-    res.json({ secret: secret });
-  } catch (err) {
-    res.send({ msg: err });
-  }
-});
+const router = express.Router();
 
-Router.post("/check", async (req, res) => {
-  try {
-    const { SECRET_CODE } = process.env;
-    const { secret } = req.body;
+router.post("/login", login);
 
-    if (secret === SECRET_CODE) {
-      res.send(true);
-    } else {
-      res.send(false);
-    }
-  } catch (err) {
-    res.send(false);
-  }
-});
-module.exports=Router;
+router.get("/logout", logout);
+
+router.get("/me", isAuthenticated, getMyProfile);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,69 +1,11 @@
-const express = require("express");
-require("dotenv/config");
-const BodyParser = require("body-parser");
-const mongoose = require("mongoose");
-// const routes=require("./routes");
-const app = express();
-const PORT = process.env.PORT;
-const cors=require("cors");
+// import { connectDB } from "./data/database.js";
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-}
+import { app } from "./app.js";
+// const app=require("./app.js")
+// connectDB();
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "Content-Type': 'application/json"
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
   );
-  next();
 });
-app.use(BodyParser.json());
-// app.use(routes);
-// const router1 = express.Router();
-// const router2 = express.Router();
-// const router3 = express.Router();
-
-// import routes
-const Movie_Data = require("./routes/movie_data");
-const Admin = require("./routes/admin");
-
-// // use routes
-app.use("/movie_data", Movie_Data);
-app.use("/admin", Admin);
-
-// con/nect database
-// 
-// CHOOSE ANY ONE
-
-mongoose.connect(  process.env.MONGO_DB_PATH,  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-},).then((res) => {
-    console.log("Database connected");
-  }).catch(error => {
-     console.log(error);
-   });
-
-// server listen on port
-
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
-});
-
-// server side cod
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
