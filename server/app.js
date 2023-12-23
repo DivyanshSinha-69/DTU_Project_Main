d4a009cbdd5f5ac936ab93f29f9d180fbaed15ae
arpangoyal <arpangoyal2002@gmail.com>
2023-12-23 22:15:36 +0530

    routes added

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 // import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
+import { connectDB } from "./data/database.js";
 
 export const app = express();
 
@@ -22,13 +23,33 @@ app.use(
 );
 
 // Using routes
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/task", taskRouter);
-// app.use("ece/admin")
+// app.use("ece/admin/login",login)
+// app.use("ece/professor/login",login)
+// app.use("ece/student/login",login)
 
 app.get("/", (req, res) => {
   res.send("Nice working");
 });
+
+app.get('/create',(req,res) => {
+  let sql = 'CREATE TABLE info(id int AUTO_INCREMENT,name varchar(255),age int, PRIMARY KEY (id))';
+  connectDB.query(sql, (err,result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('info Table Created...');
+  })
+});
+
+app.get('/add',(req,res) => {
+  let info1 = {name:'test',age:25};
+  let sql = 'INSERT INTO info SET ?';
+  let query = connectDB.query(sql,info1, (err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      res.send('Info Added');
+  })
+});
+
 
 // Using Error Middleware
 // app.use(errorMiddleware);
--- server/data/database.js
+++ server/data/database.js
@@ -2,13 +2,18 @@ import mysql from "mysql";
 
 export const connectDB = mysql.createConnection({
   host     : 'localhost',
+  // host     : process.env.HOST,
   user     : 'root',
-  password : '9868388014',
+  // user     : process.env.USE,
+  // password : process.env.PASSWORD,
+  password : "9868388014",
   database : 'admin'
 });
 
+
 // Connect to the database
 connectDB.connect((err)=>{
+    console.log("passowrd "+process.env.PASSWORD);
   if(err){
       throw err;
   }
