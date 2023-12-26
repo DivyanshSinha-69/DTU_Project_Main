4b9edbae2f61d023a7bfc7fa1be023b6bf679c3a
arpangoyal <arpangoyal2002@gmail.com>
2023-12-26 21:53:57 +0530

    agn add react

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import CandidateInfo from './Components/CandidateProfile/CandidateInfo';
-import Leftbar from './Components/CandidateProfile/Leftbar';
-import Topbar from './Components/Topbar/Topbar';
+
 function App() {
   return (
-    <div>
-      <Topbar/>
-      <Leftbar/>
-      <CandidateInfo/>
-    </div>
+    <>
+    </>
   );
 }
 
