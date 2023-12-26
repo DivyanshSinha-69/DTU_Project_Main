828a3e3b2385ebe6474d1c0b15d8fe3e377d1213
arpangoyal <arpangoyal2002@gmail.com>
2023-12-26 23:05:02 +0530

    react setup

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 function App() {
-  return (
-    <>
-    </>
+  return ( 
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
   );
 }
 
