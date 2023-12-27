cad393bc68a29ee742e2148de246400af4dbe4b8
arpangoyal <arpangoyal2002@gmail.com>
2023-12-27 18:00:29 +0530

    css

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Header from './components/Header';
 function App() {
   return ( 
     <Router>
+      <Header/>
       <Routes>
         
       </Routes>
---
+++ profile/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Hashlink} from 'react-router-hash-link'
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
