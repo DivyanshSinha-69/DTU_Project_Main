eefdce485540ad2ca7c45af38df2588a779bccd7
Divyansh Sinha <harshsinha190@gmail.com>
2025-02-05 04:33:59 +0530

    new changes

--- profile/src/components/AdminDashboard.jsx
+++ profile/src/components/AdminDashboard.jsx
@@ -304,7 +304,7 @@ const Dashboard = () => {
                               type="radio"
                               name="info"
                               onChange={handleChange}
-                              value="publicationdetails"
+                              value="publicationDetails"
                               class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
                             />
                             <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
@@ -350,7 +350,7 @@ const Dashboard = () => {
                               type="radio"
                               name="info"
                               onChange={handleChange}
-                              value="placementdata"
+                              value="placementData"
                               class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
                             />
                             <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
@@ -396,7 +396,7 @@ const Dashboard = () => {
                               type="radio"
                               onChange={handleChange}
                               name="info"
-                              value="highereducationdetails"
+                              value="higherEducationDetails"
                               class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
                             />
                             <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
