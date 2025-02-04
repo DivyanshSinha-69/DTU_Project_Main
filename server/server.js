b3be3398a6d86395c32aee09ee5b15e041581de6
Divyansh Sinha <harshsinha190@gmail.com>
2025-02-04 22:37:20 +0530

    Allowed backend access from outside droplet

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import { app } from "./app.js";
 // const app=require("./app.js")
 connectDB;
 
-app.listen(3001, () => {
+app.listen(3001, '0.0.0.0', () => {
   console.log(
     `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
   );
