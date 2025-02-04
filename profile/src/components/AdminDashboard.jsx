a2d6f10077da5afad35d0ca59cf0f9f34f27f21c
Divyansh Sinha <harshsinha190@gmail.com>
2025-02-05 03:46:59 +0530

    Corrected studentPersonalDetails

--- profile/src/components/AdminDashboard.jsx
+++ profile/src/components/AdminDashboard.jsx
@@ -7,7 +7,7 @@ import Loader from "./Loader";
 
 const Dashboard = () => {
   const [formData, setFormData] = useState({
-    info: "studentpersonaldetails",
+    info: "studentPersonalDetails",
     courseGroup: "Btech",
     year1: "2K20",
     year2: "2K25",
@@ -48,7 +48,7 @@ const Dashboard = () => {
       formData.info === "mtecheducationaldetails" &&
       formData.courseGroup === "Btech"
     ) {
-      XLSX.writeFile(wb, `btecheducationaldetails.xlsx`);
+      XLSX.writeFile(wb, `btechEducationalDetails.xlsx`);
     } else {
       XLSX.writeFile(wb, `${formData.info}.xlsx`);
     }
@@ -164,7 +164,7 @@ const Dashboard = () => {
                               id="vertical-list-react"
                               type="radio"
                               name="info"
-                              value="studentpersonaldetails"
+                              value="studentPersonalDetails"
                               defaultChecked
                               onChange={handleChange}
                               class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
