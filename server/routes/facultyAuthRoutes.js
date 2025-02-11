fe257c0440de5c09e30093dd6f605e52e2f51144
Divyansh Sinha <harshsinha190@gmail.com>
2025-02-11 23:28:36 +0530

    Updated Routes

--- server/routes/facultyAuthRoutes.js
+++ server/routes/facultyAuthRoutes.js
@@ -3,7 +3,7 @@ import { requestPasswordReset, resetPassword } from "../controllers/facultyAuthC
 
 const router = express.Router();
 
-router.post("/forgot-password", requestPasswordReset);
-router.post("/reset-password", resetPassword);
+router.post("/forgotpassword", requestPasswordReset);
+router.post("/resetpassword", resetPassword);
 
 export default router; // Ensure this is the default export
