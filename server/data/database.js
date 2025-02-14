5721894301b49431eacc8778104edec18b9aff28
Divyansh Bansal <divyanshb30@gmail.com>
2025-02-14 19:38:02 +0530

    database not conencting solved

--- server/data/database.js
+++ server/data/database.js
@@ -2,11 +2,12 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables
+console.log(DB_PASSWORD, "password");
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "12348765@",
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME || "admin",
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
